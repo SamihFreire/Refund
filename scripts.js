700efa0393b747036a8ee9c3d41f9dd9f3d1315c
samih freire <samihcisne@gmail.com>
2025-08-14 16:38:10 -0300

    feat: Formatando a moeda

--- scripts.js
+++ scripts.js
@@ -7,5 +7,20 @@ amount.oninput = () => {
     //Assim adicinando apenas números
     let value = amount.value.replace(/\D/g, "");
 
-    amount.value = value;
+    // Transforma o valor em centavos (exemplo: 150/100 = 1.5 -> Equivalente a R$ 1,50)
+    value = Number(value) / 100;
+
+    //Atualiza o valor do input
+    amount.value = formatCurrencyBRL(value);
+}
+
+function formatCurrencyBRL(value) {
+    // Formatando o valor no padrão BRL (Real Brasileiro)
+    value = value.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+    });
+
+    // Retorna o valor formatado
+    return value;
 }
