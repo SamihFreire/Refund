5813cfac034a4983eb4b4d76adf83e94b590bebe
samih freire <samihcisne@gmail.com>
2025-08-14 17:50:25 -0300

    feat: Capturando o submite do form e criado função para adicionar o elemento na lista do usuário

--- scripts.js
+++ scripts.js
@@ -1,5 +1,8 @@
 //Seleciona os elementos do formulário
+const form = document.querySelector("form");
 const amount = document.getElementById("amount");
+const expense = document.getElementById("expense");
+const category = document.getElementById("category");
 
 //Monitorando a mudança do input
 amount.oninput = () => {
@@ -23,4 +26,33 @@ function formatCurrencyBRL(value) {
 
     // Retorna o valor formatado
     return value;
+}
+
+// Captura o evento de submit do formulário para obter os valores.
+form.onsubmit = (event) => {
+    // Previne o comportamento padrão de recarregar a página
+    event.preventDefault(); 
+
+    // Cria um objeto com os detalhes da nova despesa.
+    const newExpense = {
+        id: new Date().getTime(),
+        expense: expense.value,
+        catogory_id: category.value,
+        category_name: category.options[category.selectedIndex].text, // Obtem a categoria selecionada
+        amount: amount.value,
+        created_at: new Date(),
+    }
+    // Chama a função que irá adicionar o item na lista.
+    expenseAdd(newExpense);
+}
+
+function expenseAdd(newExpense) {
+    try {
+        // Cria o elemento de li para adicionar o item (li) na lista (ul).
+        const expenseItem = document.createElement("li");
+        expenseItem.classList.add("expense");
+
+    } catch (error) {
+        alert("Não foi possível atualizar a lista de despesas.");
+    }
 }
